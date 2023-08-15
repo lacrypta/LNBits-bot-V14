1f3b18cc3aaa63f6615594dd633adea74806b4b7
ferpa <feririarte.1996@gmail.com>
2023-08-15 13:58:30 -0300

    upload config

---
+++ src/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  client: {
+    token: process.env.CLIENT_TOKEN,
+    id: process.env.CLIENT_ID,
+  },
+  handler: {
+    prefix: "?",
+    deploy: true,
+    commands: {
+      prefix: true,
+      slash: true,
+      user: true,
+      message: true,
+    },
+    mongodb: {
+      uri: process.env.MONGODB_URI,
+      toggle: true,
+    },
+  },
+};
